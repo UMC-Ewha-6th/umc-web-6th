50105ea55b60f99e43e138ae3066c9b821159b3a
LeeJooyeon <smilejuju@ewha.ac.kr>
2024-04-13 17:51:52 +0900

    feat. make components of each page

---
+++ Juju/week3/movie/src/Components/Mainpage/MainStyle.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+export const Banner = styled.div`
+position: relative;
+background: #fff7ef;
+width: 100vw;
+height: 35vh;
+`;
+
+export const BannerText = styled.div`
+position: relative;
+font-family: "Spoqa Han Sans Neo Regular";
+font-style: normal;
+font-weight: 400;
+font-size: 28px;
+line-height: 38px;
+text-align: center;
+
+color: #424242;
+
+top: 50%;
+z-index: 3;
+`;
---
+++ Juju/week3/movie/src/Components/Upcoming/Upcoming.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+function Upcoming(){
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+
+export default Upcoming;
